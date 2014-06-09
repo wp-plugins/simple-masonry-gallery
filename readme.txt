a4b4f94a251c9ebe00a406effd102745117decb6
Katsushi Kawamori <Katsushi Kawamori@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-09 23:24:58 +0000

    version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/simple-masonry-gallery/trunk@929198 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== Simple Masonry Gallery ===
+Contributors: Katsushi Kawamori
+Donate link: http://gallerylink.nyanko.org/medialink/simple-masonry-gallery/
+Tags: masonry, jquery, gallery, image, images, photo, photos, picture, pictures
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Add the effect of Masonry to image.
+
+== Description ==
+
+*   Add the effect of Masonry to image.
+
+*   Japanese (ja) - [Katsushi Kawamori](http://gallerylink.nyanko.org/medialink/simple-masonry-gallery/)
+
+== Installation ==
+
+1. Upload `simple-masonry-gallery` directory to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+none
+
+== Screenshots ==
+
+1. Example
+2. Page custom meta box
+3. Post custom meta box
+4. Settings
+
+== Changelog ==
+
+= 1.0 =
+
+== Upgrade Notice ==
+
+= 1.0 =
+
---
+++ req/SimpleMasonryAdmin.php
@@ -0,0 +1,404 @@
+<?php
+/**
+ * Simple Masonry Gallery
+ * 
+ * @package    Simple Masonry Gallery
+ * @subpackage SimpleMasonryAdmin Management screen
+    Copyright (c) 2014- Katsushi Kawamori (email : dodesyoswift312@gmail.com)
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; version 2 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+class SimpleMasonryAdmin {
+
+	/* ==================================================
+	 * Add a "Settings" link to the plugins page
+	 * @since	1.0
+	 */
+	function settings_link( $links, $file ) {
+		static $this_plugin;
+		if ( empty($this_plugin) ) {
+			$this_plugin = SIMPLEMASONRY_PLUGIN_BASE_FILE;
+		}
+		if ( $file == $this_plugin ) {
+			$links[] = '<a href="'.admin_url('options-general.php?page=simplemasonry').'">'.__( 'Settings').'</a>';
+		}
+			return $links;
+	}
+
+	/* ==================================================
+	 * Settings page
+	 * @since	1.0
+	 */
+	function plugin_menu() {
+		add_options_page( 'Simple Masonry Gallery Options', 'Simple Masonry Gallery', 'manage_options', 'simplemasonry', array($this, 'plugin_options') );
+	}
+
+
+	/* ==================================================
+	 * Settings page
+	 * @since	1.0
+	 */
+	function plugin_options() {
+
+		if ( !current_user_can( 'manage_options' ) )  {
+			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+		}
+
+		wp_enqueue_style( 'jquery-ui-tabs', SIMPLEMASONRY_PLUGIN_URL.'/css/jquery-ui.css' );
+		wp_enqueue_script( 'jquery-ui-tabs' );
+		wp_enqueue_script( 'jquery-ui-tabs-in', SIMPLEMASONRY_PLUGIN_URL.'/js/jquery-ui-tabs-in.js' );
+		wp_enqueue_script( 'jquery-check-selectall-in', SIMPLEMASONRY_PLUGIN_URL.'/js/jquery-check-selectall-in.js' );
+
+		if( !empty($_POST) ) { 
+			$this->options_updated();
+			$this->post_meta_updated();
+		}
+		$scriptname = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH).'?page=simplemasonry';
+
+		$simplemasonry_mgsettings = get_option('simplemasonry_mgsettings');
+		$pagemax =$simplemasonry_mgsettings[pagemax];
+
+		?>
+
+		<div class="wrap">
+		<h2>Simple Masonry Gallery</h2>
+
+	<div id="tabs">
+	  <ul>
+	    <li><a href="#tabs-1"><?php _e('Settings'); ?></a></li>
+		<li><a href="#tabs-2"><?php _e('Caution:'); ?></a></li>
+	<!--
+		<li><a href="#tabs-3">FAQ</a></li>
+	 -->
+	  </ul>
+
+	  <div id="tabs-1">
+		<div class="wrap">
+		<h2><?php _e('Settings'); ?></h2>
+			<form method="post" action="<?php echo $scriptname; ?>">
+
+			<p class="submit">
+			  <input type="submit" name="Submit" value="<?php _e('Save Changes') ?>" />
+			</p>
+
+			<p>
+			<div><?php _e('Number of titles to show to this page', 'simplemasonry'); ?>:<input type="text" name="simplemasonry_mgsettings_pagemax" value="<?php echo $pagemax; ?>" size="3" /></div>
+
+			<?php
+			$args = array(
+				'post_type' => 'any',
+				'numberposts' => -1,
+				'orderby' => 'date',
+				'order' => 'DESC',
+				'post_status' => null,
+				'post_parent' => $post->ID
+				); 
+
+			$postpages = get_posts($args);
+
+			// pagenation
+			foreach ( $postpages as $postpage ) {
+				++$pageallcount;
+			}
+			if (!empty($_GET['p'])){
+				$page = $_GET['p'];
+			} else {
+				$page = 1;
+			}
+			$count = 0;
+			$pagebegin = (($page - 1) * $pagemax) + 1;
+			$pageend = $page * $pagemax;
+			$pagelast = ceil($pageallcount / $pagemax);
+
+			?>
+			<table class="wp-list-table widefat">
+			<tbody>
+				<tr><td colspan="4">
+				<td align="right">
+				<?php $this->pagenation($page, $pagebegin, $pageend, $pagelast, $scriptname);
+				?>
+				</td>
+				</tr>
+				<tr>
+				<td align="left" valign="middle"><?php _e('Title'); ?></td>
+				<td align="left" valign="middle"><?php _e('Type'); ?></td>
+				<td align="left" valign="middle"><?php _e('Date/Time'); ?></td>
+				<td align="left" valign="middle"><?php _e('Apply'); ?><div><input type="checkbox" id="group_simplemasonry" class="checkAll"></div></td>
+				<td align="left" valign="middle"><?php echo __('Columns').__('Width').'(px)'; ?></td>
+				</tr>
+			<?php
+
+			if ($postpages) {
+				foreach ( $postpages as $postpage ) {
+					++$count;
+				    $apply = get_post_meta( $postpage->ID, 'simplemasonry_apply', true );
+				    $width = get_post_meta( $postpage->ID, 'simplemasonry_width', true );
+					if ( $pagebegin <= $count && $count <= $pageend ) {
+						$title = $postpage->post_title;
+						$link = $postpage->guid;
+						$posttype = $postpage->post_type;
+						$date = $postpage->post_date;
+					?>
+						<tr>
+							<td align="left" valign="middle"><a style="color: #4682b4;" title="<?php _e('View');?>" href="<?php echo $link; ?>" target="_blank"><?php echo $title; ?></a></td>
+							<td align="left" valign="middle"><?php echo $posttype; ?></td>
+							<td align="left" valign="middle"><?php echo $date; ?></td>
+							<td align="left" valign="middle">
+							    <input type="hidden" class="group_simplemasonry" name="simplemasonry_applys[<?php echo $postpage->ID; ?>]" value="false">
+							    <input type="checkbox" class="group_simplemasonry" name="simplemasonry_applys[<?php echo $postpage->ID; ?>]" value="true" <?php if ( $apply == true ) { echo 'checked'; }?>>
+							</td>
+							<td align="left" valign="middle">
+								<input type="text" name="simplemasonry_widths[<?php echo $postpage->ID; ?>]" value="<?php echo $width; ?>" size="4">
+							</td>
+						</tr>
+					<?php
+					} else {
+					?>
+					    <input type="hidden" name="simplemasonry_applys[<?php echo $postpage->ID; ?>]" value="<?php echo $apply; ?>">
+					<?php
+					}
+				}
+			}
+			?>
+				<tr><td colspan="3">
+				<td align="right">
+				<?php $this->pagenation($page, $pagebegin, $pageend, $pagelast, $scriptname);
+				?>
+				</td>
+				</tr>
+			</tbody>
+			</table>
+
+			<p class="submit">
+			  <input type="submit" name="Submit" value="<?php _e('Save Changes') ?>" />
+			</p>
+
+			</form>
+		</div>
+	  </div>
+
+	  <div id="tabs-2">
+		<div class="wrap">
+			<h2><?php _e('Caution:'); ?></h2>
+			<li><h3><?php _e('Meta-box of Simple Masonry Gallery will be added to [Edit Post] and [Edit Page]. Please apply it. Also, please enter the column width.', 'simplemasonry'); ?></h3></li>
+			<img src = "<?php echo SIMPLEMASONRY_PLUGIN_URL.'/png/apply-width.png'; ?>">
+		</div>
+	  </div>
+
+	<!--
+	  <div id="tabs-3">
+		<div class="wrap">
+		<h2>FAQ</h2>
+
+		</div>
+	  </div>
+	-->
+
+	</div>
+
+		</div>
+		<?php
+	}
+
+	/* ==================================================
+	 * Pagenation
+	 * @since	1.0
+	 * string	$page
+	 * string	$pagebegin
+	 * string	$pageend
+	 * string	$pagelast
+	 * string	$scriptname
+	 * return	$html
+	 */
+	function pagenation($page, $pagebegin, $pageend, $pagelast, $scriptname){
+
+			$pageprev = $page - 1;
+			$pagenext = $page + 1;
+			?>
+<div class='tablenav-pages'>
+<span class='pagination-links'>
+<?php if ( $page <> 1 ){
+		?><a title='<?php _e('Go to the first page'); ?>' href='<?php echo $scriptname; ?>'>&laquo;</a>
+		<a title='<?php _e('Go to the previous page'); ?>' href='<?php echo $scriptname.'&p='.$pageprev ; ?>'>&lsaquo;</a>
+<?php }	?>
+<?php echo $page; ?> / <?php echo $pagelast; ?>
+<?php if ( $page <> $pagelast ){
+		?><a title='<?php _e('Go to the next page'); ?>' href='<?php echo $scriptname.'&p='.$pagenext ; ?>'>&rsaquo;</a>
+		<a title='<?php _e('Go to the last page'); ?>' href='<?php echo $scriptname.'&p='.$pagelast; ?>'>&raquo;</a>
+<?php }	?>
+</span>
+</div>
+			<?php
+
+	}
+
+	/* ==================================================
+	 * Update wp_options table.
+	 * @since	1.0
+	 */
+	function options_updated(){
+
+		$mgsettings_tbl = array(
+						'pagemax' => intval($_POST['simplemasonry_mgsettings_pagemax'])
+						);
+		update_option( 'simplemasonry_mgsettings', $mgsettings_tbl );
+
+	}
+
+	/* ==================================================
+	 * Update wp_postmeta table for admin settings.
+	 * @since	1.0
+	 */
+	function post_meta_updated() {
+
+		$simplemasonry_applys = $_POST['simplemasonry_applys'];
+		$simplemasonry_widths = $_POST['simplemasonry_widths'];
+
+		foreach ( $simplemasonry_applys as $key => $value ) {
+			if ( $value === 'true' ) {
+		    	update_post_meta( $key, 'simplemasonry_apply', $value );
+				if( empty($simplemasonry_widths[$key]) ) { $simplemasonry_widths[$key] = 200; }
+		    	update_post_meta( $key, 'simplemasonry_width', $simplemasonry_widths[$key] );
+			} else {
+				delete_post_meta( $key, 'simplemasonry_apply' );
+				delete_post_meta( $key, 'simplemasonry_width' );
+			}
+		}
+
+	}
+
+	/* ==================================================
+	 * Add custom box.
+	 * @since	1.0
+	 */
+	function add_apply_simplemasonry_custom_box() {
+	    add_meta_box('simplemasonry', 'Simple Masonry Gallery', array(&$this,'apply_simplemasonry_custom_box'), 'page', 'side', 'high');
+	    add_meta_box('simplemasonry', 'Simple Masonry Gallery', array(&$this,'apply_simplemasonry_custom_box'), 'post', 'side', 'high');
+	}
+	 
+	/* ==================================================
+	 * Custom box.
+	 * @since	1.0
+	 */
+	function apply_simplemasonry_custom_box() {
+
+		$simplemasonry_apply = get_post_meta( $_GET['post'], 'simplemasonry_apply' );
+		$simplemasonry_width = get_post_meta( $_GET['post'], 'simplemasonry_width' );
+
+		?>
+		<table>
+		<tbody>
+			<tr>
+				<td>
+					<div>
+						<input type="radio" name="simplemasonry_apply" value="true" <?php if ($simplemasonry_apply[0] === 'true') echo 'checked'; ?>><?php _e('Apply'); ?>&nbsp;&nbsp;
+						<input type="radio" name="simplemasonry_apply" value="false" <?php if ($simplemasonry_apply[0] <> 'true') echo 'checked'; ?>><?php _e('None'); ?>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div>
+						<?php echo __('Columns').__('Width'); ?><input type="text" name="simplemasonry_width" value="<?php echo $simplemasonry_width[0]; ?>" size="4">px
+					</div>
+				</td>
+			</tr>
+		</tbody>
+		</table>
+		<?php
+
+	}
+
+	/* ==================================================
+	 * Update wp_postmeta table.
+	 * @since	1.0
+	 */
+	function save_apply_simplemasonry_postdata( $post_id ) {
+
+		$dataapply = $_POST['simplemasonry_apply'];
+		if ( $dataapply === 'true' ) {
+			add_post_meta( $post_id, 'simplemasonry_apply', $dataapply, true );
+			$datawidth = $_POST['simplemasonry_width'];
+			if ( empty($datawidth) ) { $datawidth = 200; }
+			if ( "" == get_post_meta( $post_id, 'simplemasonry_width') ) {
+				add_post_meta( $post_id, 'simplemasonry_width', $datawidth, true );
+			} else if ( $datawidth != get_post_meta( $post_id, 'simplemasonry_width' ) ) {
+				update_post_meta( $post_id, 'simplemasonry_width', $datawidth );
+			}
+		} else if ( $dataapply === '' || $dataapply === 'false' ) {
+			delete_post_meta( $post_id, 'simplemasonry_apply' );
+			delete_post_meta( $post_id, 'simplemasonry_width' );
+		}
+
+	}
+
+	/* ==================================================
+	 * Posts columns menu
+	 * @since	1.0
+	 */
+	function posts_columns_simplemasonry($columns){
+	    $columns['column_simplemasonry'] = __('Simple Masonry Gallery');
+	    return $columns;
+	}
+
+	/* ==================================================
+	 * Posts columns
+	 * @since	1.0
+	 */
+	function posts_custom_columns_simplemasonry($column_name, $post_id){
+		if($column_name === 'column_simplemasonry'){
+			$simplemasonry_apply = get_post_meta( $post_id, 'simplemasonry_apply' );
+			$simplemasonry_width = get_post_meta( $post_id, 'simplemasonry_width' );
+			if ($simplemasonry_apply[0]){
+				?>
+				<div><?php _e('Apply'); ?></div>
+				<div><?php echo __('Columns').__('Width').'&nbsp;&nbsp;'.$simplemasonry_width[0].'px'; ?></div>
+				<?php
+			} else {
+				_e('None');
+			}
+	    }
+	}
+
+	/* ==================================================
+	 * Pages columns menu
+	 * @since	1.0
+	 */
+	function pages_columns_simplemasonry($columns){
+	    $columns['column_simplemasonry'] = __('Simple Masonry Gallery');
+	    return $columns;
+	}
+
+	/* ==================================================
+	 * Pages columns
+	 * @since	1.0
+	 */
+	function pages_custom_columns_simplemasonry($column_name, $post_id){
+		if($column_name === 'column_simplemasonry'){
+			$simplemasonry_apply = get_post_meta( $post_id, 'simplemasonry_apply' );
+			$simplemasonry_width = get_post_meta( $post_id, 'simplemasonry_width' );
+			if ($simplemasonry_apply[0]){
+				?>
+				<div><?php _e('Apply'); ?></div>
+				<div><?php echo __('Columns').__('Width').'&nbsp;&nbsp;'.$simplemasonry_width[0].'px'; ?></div>
+				<?php
+			} else {
+				_e('None');
+			}
+	    }
+	}
+
+}
+
+?>
